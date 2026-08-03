fbfc24376c425e76be70439ff9eb4473e005a5ed
JORGE RICO <jorgerico@MacBook-Air-de-JORGE.local>
2026-08-03 22:52:24 +0200

    feature: llv july

--- data/tournaments/lliga_valles.py
+++ data/tournaments/lliga_valles.py
@@ -6,8 +6,8 @@
 # current league needs to be changed manually on DB when new season starts
 # ########################################################################
 # mantain array order relation
-tournament_list_mtgtop8  = [ 86604 , 84574 , 83134  , 81664 ]
-tournament_list_mtgdecks = [ 254859, 247046, 241991 , 237093 ]
+tournament_list_mtgtop8  = [ 89082,  86604 , 84574 , 83134  , 81664 ]
+tournament_list_mtgdecks = [ 263823, 254859, 247046, 241991 , 237093 ]
 
 # this league sometimes has more players loaded on mtgdecks website
 isMtgDecks = False
